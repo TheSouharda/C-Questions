b97a19c6c6e1170caf371c9a1aadce9f7bbbf9cd
Souharda Biswas <mail2souharda2015@gmail.com>
2021-03-05 23:39:13 +0530

    100doc_Od23

--- Arrays/pairTargetSum.cpp
+++ Arrays/pairTargetSum.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 int main()
 {
+    int num = 0;
+    cout << "Enter a sorted array " << endl;
+    cin >> num;
+    int arr[num];
+
+    int beg = 0, end = 0;
+
+    for (int i = 0; i < num; i++)
+    {
+
+        cin >> arr[i];
+    }
+
+    beg = 0;
+    end = n - 1;
+    while (beg != end)
+    {
+        if (arr[beg] + arr[end])
+        {
+            /* code */
+        }
+    }
 
     return 0;
 }
